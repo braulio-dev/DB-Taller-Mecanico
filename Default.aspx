6ea2758da84c977fdae111a6906e161ba1c66d0a
Braulio <bchamerry@gmail.com>
2024-09-07 12:13:08 -0700

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ProyectoBD._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+        <asp:Button ID="btnBusca" runat="server" Text="Buscar" OnClick="btnBusca_Click" />
+        <asp:GridView ID="GridViewClientes" runat="server" AutoGenerateColumns="true"></asp:GridView>
+    </main>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace ProyectoBD
+{
+    public partial class _Default : Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnBusca_Click(object sender, EventArgs e)
+
+        {
+
+            // Crear la consulta SQL
+
+            string query = "SELECT * FROM cliente";
+
+
+
+            // Crear una conexión MySQL aqui deben poner la información del servidor, de la bdd, usuario y password
+
+            using (MySqlConnection connection = new MySqlConnection(Globales.connectionStringLocal))
+
+            {
+
+                // Crear un comando MySQL
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+
+                    try
+
+                    {
+
+                        // Abrir la conexión
+
+                        connection.Open();
+
+
+
+                        // Crear un adaptador de datos
+
+                        using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+
+                        {
+
+                            // Crear un DataTable para almacenar los datos
+
+                            DataTable dataTable = new DataTable();
+
+
+
+                            // Llenar el DataTable
+
+                            adapter.Fill(dataTable);
+
+
+
+                            // Asignar el DataTable al GridView
+
+                            GridViewClientes.DataSource = dataTable;
+
+                            GridViewClientes.DataBind();
+
+                        }
+
+                    }
+
+                    catch (Exception ex)
+
+                    {
+
+                        // Manejo de excepciones
+
+                        // Puedes mostrar un mensaje de error o registrar el error
+
+                        Response.Write("Error: " + ex.Message);
+
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
